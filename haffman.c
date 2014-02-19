edc61eb8e63e79fd6bbb437945faf51353a8f518
Denis Pynkin <Denis_Pynkin@epam.com>
2014-02-19 17:59:06 +0300

    Initial commit

---
+++ haffman.c
@@ -0,0 +1,16 @@
+/*
+ * =====================================================================================
+ *       Filename:  haffman.c
+ *    Description:  
+ *         Author:  Denis Pynkin (d4s), denis.pynkin@t-linux.by
+ *        Company:  t-linux.by
+ * ===================================================================================== */
+
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+
+int main( int argc, char **argv) {
+
+	return 0;
+}
