11042f4f4889a54aeca8a6017bfe3fafa8da5509
Denis Pynkin <denis_pynkin@epam.com>
2014-04-21 20:46:49 +0300

    Just a temporary commit in a middle of work with blocks

--- hblock.c
+++ hblock.c
@@ -7,7 +7,8 @@
  *
  */
 
-#include <hpb.h>
+#include <hblock.h>
+#include <netinet/in.h>
 
 /**
  * @brief Read one message from stream
@@ -30,7 +31,9 @@ hpb_t * streamreader ( int fd, uint8_t *buffer, size_t buffer_size) {
 	static size_t rd;
 	static uint32_t offset;
 
-	/* TODO: check params */
+	/* check params */
+	assert( buffer != NULL);
+	assert( buffer_size > sizeof(uint32_t));
 
 	/* Read size of message */
 	offset = 0;
@@ -65,7 +68,7 @@ hpb_t * streamreader ( int fd, uint8_t *buffer, size_t buffer_size) {
 		}
 
 		offset += rd;
-		DBGPRINT("Bytes read %d, buffer space left %d\n", rd, buffer_size-offset);
+		DBGPRINT("Bytes read %d, buffer space left %d\n", (int)rd, (int)(buffer_size-offset));
 
 		if ( (msglen - offset) <= 0 )
 			break;
@@ -75,14 +78,14 @@ hpb_t * streamreader ( int fd, uint8_t *buffer, size_t buffer_size) {
 	msg = hpb__unpack (NULL, offset, buffer);
 
 	if (msg == NULL) {
-		DBGPRINT("Error message detected\n", offset);
+		DBGPRINT("Error message detected\n");
 		return NULL;
 	}
 		
 	/* Message parsed, hooray! */
 
 
-	DBGPRINT("Successful read of message %d\n", msgcnt);
+	DBGPRINT("Successful read of message\n");
 	return msg;
 }
 
@@ -98,4 +101,172 @@ size_t streamwriter ( int fd, hblock_t *block) {
 
 	return 0;
 }
+
+
+/**
+ * @brief Create hblock structure from raw data block
+ *
+ * @param buffer Pointer to buffer
+ * @param size Size of data in buffer
+ * @param state Shows data type in buffer
+ *
+ * @return Pointer to hblock structure associated with data or NULL for error
+ */
+hblock_t *hblock_create( uint8_t *buffer, uint32_t size, hblock_state_t state ) {
+
+	hblock_t *block;
+	uint8_t *data;
+
+	block = malloc( sizeof(hblock_t));
+	if (block == NULL) {
+		DBGPRINT("Out of memory\n");
+		return NULL;
+	}
+
+	/* Block cleanup */
+	memset( block, 0, sizeof(hblock_t)); 
+
+	if (buffer == NULL) {
+		/*  Create an empty block */
+		hblock_set_state( block, EMPTY);
+		return block;
+	}
+
+
+	/* Create memory region for data from buffer */
+	data = malloc( size);
+	assert( data != NULL);
+
+	memcpy( data, buffer, size);
+
+	switch( state) {
+		case RAW_READY:
+			block->raw = data;
+			block->raw_size = size;
+			hblock_set_state( block, RAW_READY);
+			break;
+		case ZDATA_READY:
+			block->zdata = data;
+			block->zdata_size = size;
+			hblock_set_state( block, ZDATA_READY);
+			break;
+		default:
+			free( data);
+			free( block);
+			return NULL;
+	}
+
+	return block;
+}
+
+
+
+/**
+ * @brief Destroy node and associated resources
+ *
+ * @param block Pointer to block to be destroyed
+ */
+void hblock_destroy( hblock_t *block) {
+
+		if (block == NULL)
+			return NULL;
+
+		hblock_set_state( block, PROCESSING);
+		if (block->raw != NULL)
+			free( block->raw);
+
+		if (block->zdata != NULL)
+			free( block->zdata);
+
+		if (block->head != NULL)
+			htree_destroy( block->head);
+
+		free( block);
+}
+
+/**
+ * @brief Compress data block
+ *
+ * @param buffer Pointer to block with raw data
+ * @param size Size of data in buffer
+ *
+ * @return zero on success 
+ */
+int hblock_compress( hblock_t *block) {
+
+	hnode_t **dictionary; // array of pointers to hnode_t elements
+
+	/* Frequency collector */
+	uint32_t histogram[DICTSIZE];
+
+	assert( block != NULL);
+	assert( block->raw != NULL);
+
+	hblock_set_state( block, PROCESSING);
+
+	block->raw = buffer;
+	block->raw_size = size;
+
+	/* Should not happen but who cares? */
+	if (block->zdata != NULL)
+		free( block->zdata);
+
+
+	
+
+
+	hblock_set_state( block, READY);
+	return 0;
+
+}
+
+/**
+ * @brief Decompress data block
+ *
+ * @param buffer Pointer to block with compressed data and huffman tree
+ *
+ * @return zero on success 
+ */
+int hblock_decompress( hblock_t *block) {
+
+	return 0;
+}
+
+
+
+
+/**
+ * @brief Get state of block
+ *
+ * @param block Pointer to processing block
+ *
+ * @return State of block.
+ */
+hblock_state_t hblock_get_state( hblock_t *block) {
+
+	assert( block != NULL);
+
+	/* TODO: add openmp  */
+	return block->state;
+}
+
+
+/**
+ * @brief Set state for block
+ *
+ * @param block Pointer to processing block
+ * @param state Desirable state
+ *
+ * @return State of block. ERROR if can not set state for block.
+ */
+hblock_state_t hblock_set_state( hblock_t *block, hblock_state_t state) {
+
+	assert( block != NULL);
+
+	/* TODO: add openmp  */
+	block->state = state;
+
+	return block->state;
+}
+
 
--- hblock.h
+++ hblock.h
@@ -11,25 +11,99 @@
 #include <htree.h>
 
 
+/**
+ * @brief States of block processing
+ *
+ * Description of processing steps.
+ * For future parallel processing.
+ */
 enum hblock_state {
-	EMPTY,
-	PROCESSING,
-	READY
+	EMPTY,       /**< No any data available */
+	PROCESSING,  /** Block is in use. Do not touch it!  */
+	RAW_READY,   /**< Raw data available */
+	ZDATA_READY, /**< Compressed data and Huffman's tree available */
+	READY, 		 /**< All data available and Huffman's tree is ready */
+	ERROR        /**< Error. If any.  */
 };
 
+typedef enum hblock_state hblock_state_t;
+
+
+/**
+ * @brief Structure for data management
+ *
+ * Contain all needed information for independent processing:
+ * raw and compressed data, associated Huffman tree
+ */
 struct hblock {
-	hblock_state state;
+	hblock_state_t state;
 	uint8_t   * raw; /**< Raw (uncompressed data) */
 	uint32_t  raw_size; /**< Raw data size */
-	uint8_t   * zdata; /**< Compressed with Huffman's algorithm data */
+	uint8_t   * zdata; /**< Compressed data with Huffman's algorithm */
 	uint32_t  zdata_size; /**< Compressed data size */
-	htree_t *head; /**< Pointer to head of Huffman tree */ 
+	hnode_t *head; /**< Pointer to head of Huffman tree */ 
 };
 
 typedef struct hblock hblock_t;
 
 typedef struct _Hpb hpb_t;
 
+/**
+ * @brief Create hblock structure from raw data block
+ *
+ * @param buffer Pointer to buffer
+ * @param size Size of data in buffer
+ * @param state Shows data type in buffer
+ *
+ * @return Pointer to hblock structure associated with data or NULL for error
+ */
+hblock_t *hblock_create( uint8_t *buffer, uint32_t size, hblock_state_t state );
+
+/**
+ * @brief Destroy node and associated resources
+ *
+ * @param block Pointer to block to be destroyed
+ */
+void hblock_destroy( hblock_t *block);
+
+/**
+ * @brief Get state of block
+ *
+ * @param block Pointer to block
+ *
+ * @return State of block.
+ */
+hblock_state_t hblock_get_state( hblock_t *block);
+
+
+/**
+ * @brief Set state for block
+ *
+ * @param block Pointer to processing block
+ * @param state Desirable state
+ *
+ * @return State of block. ERROR if can not set state for block.
+ */
+hblock_state_t hblock_set_state( hblock_t *block, hblock_state_t state);
+
+/**
+ * @brief Compress data block
+ *
+ * @param buffer Pointer to block with raw data
+ * @param size Size of data in buffer
+ *
+ * @return zero on success 
+ */
+int hblock_compress( hblock_t *block);
+
+/**
+ * @brief Decompress data block
+ *
+ * @param buffer Pointer to block with compressed data and huffman tree
+ *
+ * @return zero on success 
+ */
+int hblock_decompress( hblock_t *block);
 
 /**
  * @brief Read one message from stream
@@ -57,4 +131,6 @@ hpb_t * streamreader ( int fd, uint8_t *buffer, size_t buffer_size);
  * @return Size of data written or 0 on error
  */
 size_t streamwriter ( int fd, hblock_t *block);
+
+
 
